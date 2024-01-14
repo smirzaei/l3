9d9f7e39372799cbfd4602d5ff39044a081e9f17
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2024-01-14 11:44:10 -0500

    feat(frame): check version

--- src/frame.rs
+++ src/frame.rs
@@ -1,3 +1,12 @@
+use anyhow::Result;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum FrameError {
+    #[error("invalid version {0} (expected 1)")]
+    InvalidVersion(u8),
+}
+
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct Frame {
     version: u8,
@@ -9,27 +18,49 @@ pub struct Frame {
 }
 
 impl Frame {
-    pub fn from_bytes(buff: &[u8; 8]) -> Self {
-        Frame {
-            version: buff[0],
+    pub fn from_bytes(buff: &[u8; 8]) -> Result<Self> {
+        let version = buff[0];
+        if version != 1 {
+            return Err(FrameError::InvalidVersion(version).into());
+        }
+
+        let msg_length = ((buff[4] as u32)
+            | ((buff[5] as u32) << 8)
+            | ((buff[6] as u32) << 16)
+            | ((buff[7] as u32) << 24));
+
+        Ok(Frame {
+            version,
             p1: buff[1],
             p2: buff[2],
             p3: buff[3],
-            msg_length: ((buff[4] as u32)
-                | ((buff[5] as u32) << 8)
-                | ((buff[6] as u32) << 16)
-                | ((buff[7] as u32) << 24)),
-        }
+            msg_length,
+        })
     }
 }
 
 #[cfg(test)]
 mod test {
+    use crate::frame::FrameError;
+
     use super::Frame;
+    use anyhow::{Ok, Result};
 
     #[test]
-    fn from_bytes_properly_constructs_a_frame() {
-        let b: [u8; 8] = [0x1, 0x2, 0x3, 0x4, 0x05, 0x06, 0x07, 0x08];
+    fn from_bytes_return_error_if_version_is_not_one() -> Result<()> {
+        let b: [u8; 8] = [0x00, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08];
+        let result = Frame::from_bytes(&b);
+        match result.unwrap_err().downcast::<FrameError>().unwrap() {
+            FrameError::InvalidVersion(0) => Ok(()),
+            _ => {
+                panic!("invalid error");
+            }
+        }
+    }
+
+    #[test]
+    fn from_bytes_properly_constructs_a_frame() -> Result<()> {
+        let b: [u8; 8] = [0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08];
         let expected = Frame {
             version: 1,
             p1: 2,
@@ -39,8 +70,9 @@ mod test {
                 .to_le(),
         };
 
-        let result = Frame::from_bytes(&b);
+        let result = Frame::from_bytes(&b)?;
 
         assert_eq!(expected, result);
+        Ok(())
     }
 }
